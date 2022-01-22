aed0b31668f1e371fdcbc545b5bd356ec99f9541
Valence Kajuna <60002218+Valence-Kajuna@users.noreply.github.com>
2022-01-22 15:54:56 +0300

    Created initial entry point file

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+//Load env variables
+dotenv.config({ path: './config/config.env' });
+
+// Create and express app
+const app = express();
+
+// Make an app listen to port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
