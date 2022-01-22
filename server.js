8f93c6560e11eda48e45b6055034524c92ca8156
Valence Kajuna <60002218+Valence-Kajuna@users.noreply.github.com>
2022-01-22 16:13:38 +0300

    made the port to 5000

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ dotenv.config({ path: './config/config.env' });
 const app = express();
 
 // Make an app listen to port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
