e610e73d4d584881158168586f5829ef48a9d62e
Valence Kajuna <60002218+Valence-Kajuna@users.noreply.github.com>
2022-01-22 16:40:49 +0300

    created some routes for resource bootcamp

--- server.js
+++ server.js
@@ -7,12 +7,53 @@ dotenv.config({ path: './config/config.env' });
 // Create and express app
 const app = express();
 
-// Creating sample route
+//Get all bootcamps
 app.get('/api/v1/bootcamps', (req, res)=>{
     res.status(200).json(
         {
             "success":true,
             "msg": "Show all bootcamps"
+        }
+    )
+})
+
+// Get a single bootcamp
+app.get('api/v1/bootcamps/:id', (req,res)=>{
+    res.status(200).json(
+        {
+            "success": true,
+            "msg": `Bootcamp with id ${req.params.id}`
+        }
+    )
+})
+
+// Create a bootcamp
+app.post('api/v1/bootcamps', (req,res)=>{
+    res.status(200).json(
+        {
+            "success": true,
+            "msg": `Created a new bootcamp`
+        }
+    )
+})
+
+// Edit a bootcamp
+app.put('api/v1/bootcamps/:id', (req,res)=>{
+    res.status(200).json(
+        {
+            "success": true,
+            "msg": `Edited bootcamp with id ${req.params.id}`
+        }
+    )
+})
+
+
+// Delete a bootcamp
+app.delete('api/v1/bootcamps/:id', (req,res)=>{
+    res.status(200).json(
+        {
+            "success": true,
+            "msg": `Deleted bootcamp ${req.params.id}`
         }
     )
 })
