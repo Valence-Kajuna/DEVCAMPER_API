1d47fa79552c014a78e9940797e886e85589bdef
Valence Kajuna <60002218+Valence-Kajuna@users.noreply.github.com>
2022-01-22 16:45:08 +0300

    added the bootcamps routes in a seperate file

--- server.js
+++ server.js
@@ -7,57 +7,6 @@ dotenv.config({ path: './config/config.env' });
 // Create and express app
 const app = express();
 
-//Get all bootcamps
-app.get('/api/v1/bootcamps', (req, res)=>{
-    res.status(200).json(
-        {
-            "success":true,
-            "msg": "Show all bootcamps"
-        }
-    )
-})
-
-// Get a single bootcamp
-app.get('/api/v1/bootcamps/:id', (req,res)=>{
-    res.status(200).json(
-        {
-            "success": true,
-            "msg": `Bootcamp with id ${req.params.id}`
-        }
-    )
-})
-
-// Create a bootcamp
-app.post('/api/v1/bootcamps', (req,res)=>{
-    res.status(200).json(
-        {
-            "success": true,
-            "msg": `Created a new bootcamp`
-        }
-    )
-})
-
-// Edit a bootcamp
-app.put('/api/v1/bootcamps/:id', (req,res)=>{
-    res.status(200).json(
-        {
-            "success": true,
-            "msg": `Edited bootcamp with id ${req.params.id}`
-        }
-    )
-})
-
-
-// Delete a bootcamp
-app.delete('/api/v1/bootcamps/:id', (req,res)=>{
-    res.status(200).json(
-        {
-            "success": true,
-            "msg": `Deleted bootcamp ${req.params.id}`
-        }
-    )
-})
-
 // Make an app listen to port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
