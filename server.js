8e69cf86d7aa4d439c8f7562e365c3c3b6c4240a
Valence Kajuna <60002218+Valence-Kajuna@users.noreply.github.com>
2022-01-22 16:17:47 +0300

    added a simple route

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ dotenv.config({ path: './config/config.env' });
 // Create and express app
 const app = express();
 
+// Creating sample route
+app.get('/', (req, res)=>{
+    res.send("Hello from express!")
+})
+
 // Make an app listen to port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
