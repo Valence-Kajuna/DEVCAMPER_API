3fd9604b90d2dda1106ef61d3b103951a08f35c5
Valence Kajuna <60002218+Valence-Kajuna@users.noreply.github.com>
2022-01-30 18:34:11 +0300

    created bootcamp schema

--- models/Course.js
+++ models/Course.js
@@ -13,5 +13,39 @@ const CourseSchema =  new mongoose.Schema(
         description: {
             type: String,
             required: [true, 'Please add course description']
-    }
-)
+        },
+        weeks: {
+            type: Number,
+            required: [true,'Please add number of weeks']
+        },
+        tuition: {
+            type: Number,
+            required: [true,'Please add a tuition cost']
+        },
+        minimumSkill: {
+            type: String,
+            required: [true,'Please add a minimum skill'],
+            enum : ['beginner', 'intermediate', 'advanced']
+        },
+        minimumSkill: {
+            type: String,
+            required: [true,'Please add a minimum skill'],
+            enum : ['beginner', 'intermediate', 'advanced']
+        },
+        ScholarshipAvailable:{
+            type: Boolean,
+            default: false
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        bootcamp:{
+            type: mongoose.Schema.ObjectId,
+            ref: 'Bootcamp',
+            required: true
+        }
+}
+);
+
+module.exports = mongoose.model('Course',CourseSchema)
