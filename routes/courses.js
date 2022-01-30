4ce0b9ff52b2d87d412f6db1efd8fe6adfd4ab06
Valence Kajuna <60002218+Valence-Kajuna@users.noreply.github.com>
2022-01-30 23:43:29 +0300

    updated courses routes

--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const {getCourses, getSingleCourse, createNewCourse} = require('../controllers/courses');
+const {getCourses, getSingleCourse, createNewCourse,updateCourse} = require('../controllers/courses');
 
 const router = express.Router({mergeParams: true});
 
 router.route('/').get(getCourses).post(createNewCourse);
-router.route('/:id').get(getSingleCourse);
+router.route('/:id').get(getSingleCourse).put(updateCourse);
 
 module.exports =  router
