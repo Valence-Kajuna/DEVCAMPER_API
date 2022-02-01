ce6045f0f16caebfabc3dbcdacbef144f727c400
Valence Kajuna <60002218+Valence-Kajuna@users.noreply.github.com>
2022-02-01 12:10:28 +0300

    saved data to database

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,16 @@ const User = require('../models/User');
 
 exports.register = async (req, res, next) => {
     try {
-        res.status(200).json({ success: true});
+        const { name, email, password, role } = req.body;
+        
+        // Create user
+        const user = await User.create({
+            name,
+            email,
+            password,
+            role
+        })
+        res.status(200).json({ success: true, });
     } catch (error) {
         next(error);
     }
