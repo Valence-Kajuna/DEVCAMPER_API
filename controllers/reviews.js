b0247e390e2d5299e2df5177d82d5857e1554ad8
Valence Kajuna <60002218+Valence-Kajuna@users.noreply.github.com>
2022-02-02 10:46:38 +0300

    controller to get a single review

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -26,3 +26,32 @@ exports.getReviews = async (req,res,next) =>{
         next(error)
     }
 }
+
+
+
+//@desc     Get a Single Review
+//@Method   Get
+//@route    /api/v1/reviews/:id
+//@access   Public
+exports.getReview = async (req,res,next) =>{
+    try {
+       const review = await Review.findById(req.params.id).populate(
+           {
+               path: 'bootcamp',
+               select: 'name description'
+           }
+       );
+
+       if(!review){
+           return next(new ErrorResponse('Review not found'), 404);
+       }
+
+         res.status(200).json({
+                success: true,
+                data: review
+            });
+            
+    } catch (error) {
+        next(error)
+    }
+}
