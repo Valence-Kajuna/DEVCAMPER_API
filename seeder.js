a289f89cccfe4799547f74e169aabf759b6aed47
Valence Kajuna <60002218+Valence-Kajuna@users.noreply.github.com>
2022-02-02 11:01:23 +0300

    updated the seeder to add reviews

--- seeder.js
+++ seeder.js
@@ -18,17 +18,20 @@ mongoose.connect(process.env.MONGO_URI, {
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
 const User = require('./models/User');
-
+const Review = require('./models/Review');
 // Read a json file for bootcamps
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`));
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`));
 const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/_data/reviews.json`));
 // Import data
  const importData =  async ()=>{
      try {
          await Bootcamp.create(bootcamps);
          await Course.create(courses)
          await User.create(users);
+         await Review.create(reviews);
+
          console.log('Data Imported'.green.inverse)
          process.exit();
      } catch (error) {
@@ -43,6 +46,7 @@ const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`));
          await Bootcamp.deleteMany();
          await Course.deleteMany();
          await User.deleteMany();
+         await Review.deleteMany();
          console.log('Data deleted'.red.inverse);
         process.exit();
      } catch (error) {
