833066e6984c36e35dbc1c95a50069ae807f5c13
Valence Kajuna <60002218+Valence-Kajuna@users.noreply.github.com>
2022-02-02 17:24:32 +0300

    message on error midofied

--- middleware/error.js
+++ middleware/error.js
@@ -10,7 +10,7 @@ const errorHandler = (err,req,res,next)=>{
 
     // Dealing with Mongoose bad Object ID
     if(err.name === 'CastError'){
-        const message = `Resource not found with id of ${err.value}`;
+        const message = `Resource not found`;
         error = new ErrorResponse(message,404);
     }
 
