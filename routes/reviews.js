671d98fc2e453b6f53a545fffe87de8ff450fea3
Valence Kajuna <60002218+Valence-Kajuna@users.noreply.github.com>
2022-02-02 11:35:16 +0300

    configured the create review router

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router({mergeParams: true});
 const Review = require('../models/Review');
 const Bootcamp = require('../models/Bootcamp');
-const {getReviews, getReview} = require('../controllers/reviews');
+const {getReviews, getReview, createReview} = require('../controllers/reviews');
 const advancedResults = require('../middleware/advancedResults');
+const {protect, authorize} = require('../middleware/auth');
 
 router.route('/')
     .get(advancedResults(Review,{
     path: 'bootcamp',
-    select: 'name description'}),getReviews);
+    select: 'name description'}),getReviews)
+    .post(protect,authorize('user', 'admin'), createReview);
 
 router.route('/:id')
     .get(getReview);
