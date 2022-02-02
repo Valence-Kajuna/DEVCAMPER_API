dfe555a99293ea19640c61b9cc11c4bd42064994
Valence Kajuna <60002218+Valence-Kajuna@users.noreply.github.com>
2022-02-02 17:08:06 +0300

    logout functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -226,3 +226,24 @@ exports.updatePassword = async (req, res, next) => {
     }
 
 }
+
+//@desc     Logout user
+//@Method   Get
+//@route    /api/v1/auth/logout
+//@access   Private
+exports.logout = async (req, res, next) => {
+    try {
+        res.cookie('token','none',{
+            expires:  new Date(Date.now + 10 *1000 ),
+            httpOnly: true
+        })
+        res.status(200).json({
+            success: true,
+            data: {}
+        });
+
+    } catch (error) {
+        next(error);
+    }
+
+}
