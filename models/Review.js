c3662a533fc001a261113323e96f46381255a819
Valence Kajuna <60002218+Valence-Kajuna@users.noreply.github.com>
2022-02-02 11:36:12 +0300

    fixed the true bug

--- models/Review.js
+++ models/Review.js
@@ -39,6 +39,6 @@ const ReviewSchema =  new mongoose.Schema(
 );
 
 // Prevent user from submitting more than one review per bootcamps
-ReviewSchema.index({bootcamp:1, user:1}, {unique: trues});
+ReviewSchema.index({bootcamp:1, user:1}, {unique: true});
 
 module.exports = mongoose.model('Review', ReviewSchema);
